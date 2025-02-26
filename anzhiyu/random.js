1936dbd27a633ccf8cf73e2a2572192792b9ef43
xiaoxiaoheiyu <xiaoxiaoheiyu@users.noreply.github.com>
2025-02-26 13:48:31 +0000

    deploy: xiaoxiaoheiyu/Hexo@6d722a2e5abe6f9b51c3ab498a59a95ff54726df

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/26/hello-world/","2025/02/26/test/"];function toRandomPost(){
+var posts=["2025/02/26/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
